e882fbe67bc07280206a9071c438561bbce4272b
franky4p_mac <franky4p@yandex.ru>
2020-10-27 20:58:56 +0300

    Task2

---
+++ Modul_2/Task2/Task1/ViewControllerMain.swift
@@ -0,0 +1,18 @@
+//
+//  ViewControllerMain.swift
+//  Task1
+//
+//  Created by macbook on 27.10.2020.
+//
+
+import UIKit
+
+class ViewControllerMain: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
