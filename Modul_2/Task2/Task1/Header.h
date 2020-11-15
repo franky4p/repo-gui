6960838d4c664f55c54ba067879b9c44af4436f3
franky4p_mac <franky4p@yandex.ru>
2020-11-16 00:03:57 +0300

    Task 6

---
+++ Modul_2/Task2/Task1/Header.h
@@ -0,0 +1,12 @@
+//
+//  Header.h
+//  Task1
+//
+//  Created by macbook on 15.11.2020.
+//
+
+#ifndef Header_h
+#define Header_h
+
+
+#endif /* Header_h */
---
+++ Modul_2/Task2/Task1/Item1/TableViewHeader.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewHeader.swift
+//  Task1
+//
+//  Created by macbook on 15.11.2020.
+//
+
+import UIKit
+
+class TableViewHeader: UITableViewHeaderFooterView {
+
+    
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
