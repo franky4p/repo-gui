a1dc13dc9b24623cb0f4e7c98352bbf76a4c37e0
franky4p_mac <franky4p@yandex.ru>
2020-11-19 13:39:06 +0300

    Task 7

--- Modul_2/Task2/Task1/Item2/TableViewCellGroup.swift
+++ Modul_2/Task2/Task1/Item2/TableViewCellGroup.swift
@@ -12,15 +12,18 @@ class TableViewCellGroup: UITableViewCell {
     @IBOutlet weak var labelName: UILabel!
     @IBOutlet weak var imageGroup: UIImageView!
     
+    let animationView = UIView()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        if selected {
+            UIView.animate(withDuration: 2.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: .curveEaseOut, animations: {self.imageGroup.frame.size = CGSize(width: 70, height: 70)}, completion: {_ in self.imageGroup.transform = .identity})
+        }
     }
 
 }
--- Modul_2/Task2/Task1/ViewController.swift
+++ Modul_2/Task2/Task1/ViewController.swift
@@ -12,9 +12,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var login: UITextField!
     @IBOutlet weak var password: UITextField!
     
+    let animationView1 = UIView()
+    let animationView2 = UIView()
+    let animationView3 = UIView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(self.animationView1)
+        self.view.addSubview(self.animationView2)
+        self.view.addSubview(self.animationView3)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.animationView1.frame = CGRect(x: 100, y: 100, width: 20, height: 20)
+        self.animationView1.layer.cornerRadius = 10
+        self.animationView1.backgroundColor = .gray
+        
+        self.animationView2.frame = CGRect(x: 130, y: 100, width: 20, height: 20)
+        self.animationView2.layer.cornerRadius = 10
+        self.animationView2.backgroundColor = .gray
+        
+        self.animationView3.frame = CGRect(x: 160, y: 100, width: 20, height: 20)
+        self.animationView3.layer.cornerRadius = 10
+        self.animationView3.backgroundColor = .gray
+        
+        UIView.animate(withDuration: 0.5, delay: 0.3, options:[.autoreverse, .repeat], animations: {self.animationView1.alpha -= 1})
+        
+        UIView.animate(withDuration: 0.5, delay: 1.2, options:[.autoreverse, .repeat], animations: {self.animationView2.alpha -= 1})
+        
+        UIView.animate(withDuration: 0.5, delay: 2, options:[.autoreverse, .repeat], animations: {self.animationView3.alpha -= 1})
+        
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
@@ -22,8 +52,12 @@ class ViewController: UIViewController {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vController = storyboard.instantiateViewController(identifier: "homeController")
             
-            self.present(vController, animated: true, completion: nil)
+            UIApplication.shared.windows.first?.rootViewController = vController
         }
     }
+    
+   
+    
+    
 }
 
